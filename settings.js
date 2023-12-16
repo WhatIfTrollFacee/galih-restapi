536104d799ea9c874097e6af736c49aff5b50f24
WhatIfTrollFacee <140618835+WhatIfTrollFacee@users.noreply.github.com>
2023-12-17 02:05:42 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,11 +1,11 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
-global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
-global.limitCount = 10000
+global.creator = 'galih'// yourname
+global.MONGO_DB_URI = "mongodb+srv://galdev:<password>@data-apigaldev.s8htncb.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
+global.ACTIVATION_TOKEN_SECRET = "galih" //isi apa aja bebas
+global.your_email = "mirhamgunawan02@gmail.com" //email
+global.email_password = "qfbqycpcgnanvlqy" //application password email
+global.limitCount = 9999999
 global.YUOR_PORT = 8000
 global.loghandler = {
 	noapikey:{
